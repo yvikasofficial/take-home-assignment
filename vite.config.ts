2986496ffa3d920ebf26148785091bbd477bc5fb
yvikasofficial <yvikasofficial@gmail.com>
2025-06-13 19:35:46 -0400

    added vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
